95218dea6c3d2a0c0ecb0e777a0335ac4bbe72d4
MikaAK <mikakalathil@gmail.com>
2015-06-03 16:53:50 -0700

    remove test

--- test.js
+++
@@ -1,6 +0,0 @@
-process.setMaxListeners(0)
-require('./edvisor_search')('international school agency')
-  .then(function(data) {
-    console.log('Fetched Emails: ', data.emails.length)
-    console.log('Fetched Links: ', data.links.length)
-  })
