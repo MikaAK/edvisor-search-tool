b57c025efc1798527ccf80438901488f00d053e6
MikaAK <mikakalathil@gmail.com>
2015-06-03 17:31:14 -0700

    Update everything

--- search.js
+++ search.js
@@ -1,23 +1,37 @@
 #!/usr/bin/env node
 
-var cli = require('cli'),
-    search = require('./lib/edvisor_search')
+var cli    = require('cli'),
+    search = require('./lib/edvisor_search'),
+    fs     = require('fs')
+
+cli.parse({
+  pages: ['p', 'Number of pages to google', 'number', 1]
+})
 
 cli.main(function(args, opts) {
   if (!args.length)
     cli.fatal("You must provide a search query")
 
-  search(args[0])
+  search(args[0], opts.pages)
     .then(function(data) {
-      console.log('In then', data)
       cli.info('Fetched Emails: ' + data.emails.length)
       cli.info('Fetched Links: '  + data.links.length)
 
       return data.emails
     })
     .then(function(emails) {
-      cli.ok('Complete')
-      console.log(emails)
+      var file = fs.createWriteStream('emails.csv')
+
+      file.write('Email\n')
+
+      emails.forEach(function(email) {
+        file.write(email + '\n')
+      })
+
+      file.end()
+    })
+    .then(function() {
+      cli.ok('Finished Writing To File')
     })
     .catch(function(e) {
       cli.fatal(e)
